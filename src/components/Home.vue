c5f9de6a6a5c6d55e286ba7e33711a2f7af0b037
91alban <aloku93@gmail.com>
2021-11-17 12:33:01 +0100

    added a header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
+    <Header />
     <h1>
         Hello User, Welcome on HomePage        
     </h1>
 </template>
 
 <script>
+import Header from './Header.vue';
 export default {
     name: 'Home',
+    components: {
+        Header
+    },
     mounted() {
         let user = localStorage.getItem('user-info');
         if (!user) {
