068c9ba70711cefa15664fd3645e5a8610f1f131
91alban <aloku93@gmail.com>
2021-11-17 11:40:50 +0100

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>
+        Hello User, Welcome on HomePage        
+    </h1>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,74 @@
+<template>
+    <img src="../assets/logo-light.png" class="logo">
+    <h1>
+        Sign Up        
+    </h1>
+    <div class="register">
+        <input type="text" v-model="name" placeholder="Enter Name" />
+        <input type="text" v-model="email" placeholder="Enter Email" />
+        <input type="password" v-model="password" placeholder="Enter Password" />
+        <button @click="signUp">Sign Up</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Sign Up',
+    data() {
+        return {
+            name: '',
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async signUp() {
+            let result = await axios.post("http://localhost:3000/users",{
+                email:this.email,
+                password:this.password,
+                name:this.name
+            });
+
+            console.warn(result);
+            if(result.status == 201){
+                localStorage.setItem("user-info", JSON.stringify(result.data))
+                this.$router.push({name: 'Home'})
+            }
+        }
+    },
+    mounted() {
+        let user = localStorage.getItem('user-info');
+        if (user) {
+            this.$router.push({name: 'Home'})
+        }
+    }
+}
+</script>
+
+<style>
+.logo {
+    /* width: 100%; */
+    background-color: skyblue;
+}
+
+.register input{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+}
+
+.register button {
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
