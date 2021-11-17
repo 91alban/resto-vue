3fd072e77ddbf365412f5ea65196f2f42f90161d
91alban <aloku93@gmail.com>
2021-11-17 15:40:02 +0100

    add new resto

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@ body {
     background-color: skyblue;
 }
 
-.register input, .login input{
+.register input, .login input, .add input{
     width: 300px;
     height: 40px;
     padding-left: 20px;
@@ -43,7 +43,7 @@ body {
     border: 1px solid skyblue;
 }
 
-.register button, .login button {
+.register button, .login button, .add button {
     width: 320px;
     height: 40px;
     border: 1px solid skyblue;
