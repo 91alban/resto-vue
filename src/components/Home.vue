be53a4553ace360a8aec970f2e94a1fef4d46992
91alban <aloku93@gmail.com>
2021-11-17 17:01:08 +0100

    finished the course

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,12 +9,17 @@
             <td>Name</td>
             <td>Contact</td>
             <td>Address</td>
+            <td>Actions</td>
         </tr>
         <tr v-for="item in restaurnat" :key="item.id">
             <td>{{ item.id }}</td>
             <td>{{ item.name }}</td>
             <td>{{ item.contact }}</td>
             <td>{{ item.address }}</td>
+            <td>
+                <router-link :to="'/update/' +item.id">Update</router-link>
+                <button @click="deleteRestaurant(item.id)">Delete</button>    
+            </td>
         </tr>
     </table>
 </template>
@@ -33,16 +38,28 @@ export default {
     components: {
         Header
     },
-    async mounted() {
-        let user = localStorage.getItem('user-info');
-        this.name = JSON.parse(user).name;
-        if (!user) {
-            this.$router.push({name: 'SignUp'})
-        }
+    methods: {
+        async deleteRestaurant(id){
+            let result = await axios.delete("http://localhost:3000/restaurant/" + id);
+            console.warn(result)
+            if(result.status == 200) {
+                this.loadData()
+            }
+        },
+        async loadData() {
+            let user = localStorage.getItem('user-info');
+            this.name = JSON.parse(user).name;
+            if (!user) {
+                this.$router.push({name: 'SignUp'})
+            }
 
-        let result = await axios.get("http://localhost:3000/restaurant");
-        console.warn(result)
-        this.restaurnat = result.data;
+            let result = await axios.get("http://localhost:3000/restaurant");
+            console.warn(result)
+            this.restaurnat = result.data;
+        }
+    },
+    async mounted() {
+        this.loadData();
     }
 }
 </script>
